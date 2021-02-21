a2261a2edd58ab08491cb30e90d49fa3a0e87270
baadjis <baadjisidy@gmail.com>
2021-02-21 17:39:58 +0100

    added readUrl functionality

--- static/js/main.js
+++ static/js/main.js
@@ -3,12 +3,27 @@ $(document).ready(function () {
     $('#submit-btn').hide();
 
 
-
     $("#imageUpload").change(function () {
+
         $('#submit-btn').show();
+        readURL(this)
 
     });
 
 
+function readURL(input) {
+if (input.files && input.files[0]) {
+    var reader = new FileReader();
+
+    reader.onload = function (e) {
+        $('#previewimage')
+            .attr('src', e.target.result)
+            .width(800)
+            .height(600);
+    };
+
+    reader.readAsDataURL(input.files[0]);
+}
+}
 
 });
